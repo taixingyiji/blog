1977041fe4452b0028681920a0a584e2349134f8
lhc <taixingyiji@126.com>
2020-12-17 17:39:44 +0800

    delete md,write a artile

--- docs/.vuepress/config/nav.js
+++ docs/.vuepress/config/nav.js
@@ -5,47 +5,20 @@ module.exports = [
     link: '/web/', //目录页链接，此处link是vdoing主题新增的配置项，有二级导航时，可以点击一级导航跳到目录页
     items: [
       // 说明：以下所有link的值只是在相应md文件定义的永久链接（不是什么特殊生成的编码）。另外，注意结尾是有斜杠的
-      {
-        text: '前端文章',
-        items: [
-          { text: 'JavaScript', link: '/pages/8143cc480faf9a11/' },
-          { text: 'Vue', link: '/pages/802a1ca6f7b71c59/' },
-        ],
-      },
-      {
-        text: '学习笔记',
-        items: [
-          { text: '《JavaScript教程》笔记', link: '/note/javascript/' },
-          { text: '《JavaScript高级程序设计》笔记', link: '/note/js/' },
-          { text: '《ES6 教程》笔记', link: '/note/es6/' },
-          { text: '《Vue》笔记', link: '/note/vue/' },
-          {
-            text: '《TypeScript 从零实现 axios》',
-            link: '/note/typescript-axios/',
-          },
-          {
-            text: '《Git》学习笔记',
-            link: '/note/git/',
-          },
-          {
-            text: '《TypeScript》笔记',
-            link: '/pages/51afd6/',
-          },
-        ],
-      },
+
     ],
   },
   {
-    text: '页面',
-    link: '/ui/',
+    text: '后台',
+    link: '/server/',
     items: [
       { text: 'HTML', link: '/pages/8309a5b876fc95e3/' },
       { text: 'CSS', link: '/pages/0a83b083bdf257cb/' },
     ],
   },
   {
-    text: '技术',
-    link: '/technology/',
+    text: '运维',
+    link: '/devops/',
     items: [
       { text: '技术文档', link: '/pages/9a7ee40fc232253e/' },
       { text: 'GitHub技巧', link: '/pages/4c778760be26d8b3/' },
