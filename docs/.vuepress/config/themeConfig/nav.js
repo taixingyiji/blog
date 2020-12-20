1990da20c847f11cd4d0d8fff53f5a327525ae2d
lhc <taixingyiji@126.com>
2020-12-20 22:35:53 +0800

    编写文章，如何让博客可以被百度收录

--- docs/.vuepress/config/themeConfig/nav.js
+++ docs/.vuepress/config/themeConfig/nav.js
@@ -4,8 +4,13 @@ module.exports = [
     text: '前端',
     link: '/web/', //目录页链接，此处link是vdoing主题新增的配置项，有二级导航时，可以点击一级导航跳到目录页
     items: [
-      // 说明：以下所有link的值只是在相应md文件定义的永久链接（不是什么特殊生成的编码）。另外，注意结尾是有斜杠的
-      { text: 'VuePress', link: '/vuepress/keywords/' },
+      {
+        text: '博客搭建',
+        items: [
+          { text: '搜索引擎收录', link: '/search/baidu/' },
+          { text: 'SEO优化', link: '/vuepress/keywords/' },
+        ],
+      },
     ],
   },
   {
